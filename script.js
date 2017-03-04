e8fbd0548988a3b17d8a8864f901c25053958ee5
Rustem Kakimov <ruskakimov@gmail.com>
2017-03-04 16:30:27 +0800

    Sync input with localStorage

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
-
-
 document.addEventListener('DOMContentLoaded', function () {
     var input = document.getElementById('task');
+
+    input.value = localStorage[input.name];
+
+    input.addEventListener('input', function (e) {
+        localStorage[e.target.name] = e.target.value;
+    });
 })
