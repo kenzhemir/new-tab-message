002e411a197dfc03be24bbd05d742ef227e0965a
Rustem Kakimov <ruskakimov@gmail.com>
2017-03-04 19:21:20 +0800

    Add default message

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     var input = document.getElementById('message');
 
-    input.value = localStorage[input.name];
+    input.value = localStorage[input.name] || 'type message here';
 
     input.addEventListener('input', function (e) {
         localStorage[e.target.name] = e.target.value;
