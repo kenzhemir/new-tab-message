2991029b3561f1d4fec83e8287d7cb000f93d49b
Rustem Kakimov <ruskakimov@gmail.com>
2017-03-05 12:51:56 +0800

    Rename var

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var input = document.getElementById('message');
+    var textField = document.getElementById('message');
 
-    input.value = localStorage[input.name] || 'type message here';
+    textField.value = localStorage[textField.name] || 'type message here';
 
-    input.addEventListener('input', function (e) {
+    textField.addEventListener('input', function (e) {
         localStorage[e.target.name] = e.target.value;
     });
 })
