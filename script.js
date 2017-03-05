a1d9dc2e3412bc25a9594d9101cd5378fde5122b
Rustem Kakimov <ruskakimov@gmail.com>
2017-03-05 16:41:56 +0800

    Migrate to chrome.storage

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var textField = document.getElementById('message');
-
-    textField.value = localStorage[textField.name] || 'click and edit this message';
-
-    textField.addEventListener('input', function (e) {
-        localStorage[e.target.name] = e.target.value;
+function loadMessage(textField) {
+    chrome.storage.sync.get(null, function(items) {
+        var loadedMessage = items[textField.name] || 'click and edit this message';
+        textField.value = loadedMessage;
     });
+}
+
+function storeMessage(event) {
+    var items = {};
+    items[event.target.name] = event.target.value;
+    chrome.storage.sync.set(items)
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    var textField = document.getElementById('message');
+    loadMessage(textField);
+    textField.addEventListener('input', storeMessage);
 })
